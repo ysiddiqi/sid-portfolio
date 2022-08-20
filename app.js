e4889b10b88ffe3ac1e803d841f476edba635de1
ysiddiqi <ysiddiqi@users.noreply.github.com>
2022-08-20 14:18:00 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((ele) => ele.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
